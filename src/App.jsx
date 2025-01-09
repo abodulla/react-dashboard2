b870255a767009c8c2ad9083bd9daecee9010447
abodulla <154546425+abodulla@users.noreply.github.com>
2025-01-09 14:40:43 +0300

    reset the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
-import SideBar from "./components/common/SideBar";
+import Sidebar from "./components/common/Sidebar";
 
-import Home from "./pages/Home";
+import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 import UsersPage from "./pages/UsersPage";
-import "./index.css";
 import SalesPage from "./pages/SalesPage";
+import OrdersPage from "./pages/OrdersPage";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
@@ -16,13 +18,15 @@ function App() {
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
 
-      <SideBar />
-
+      <Sidebar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
   );
